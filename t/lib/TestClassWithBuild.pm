357707d1d145c3ab03607691d91fc2f11db6ebe4
Torbjørn Lindahl <torbjorn.lindahl@diagenic.com>
2013-05-29 15:01:00 +0200

    testing with build

---
+++ t/lib/TestClassWithBuild.pm
@@ -0,0 +1,25 @@
+package TestClassWithBuild;
+
+use strict;
+use warnings;
+
+use Moose;
+use File::Slurp;
+
+has blob => ( isa => "Any", is => "rw" );
+
+sub parse {
+
+    my $self = shift;
+    $self->blob( read_file( $self->fh ));
+
+}
+
+sub BUILD {
+    my $self = shift;
+    print $self->filename, "\n";
+}
+
+with "MooseX::FileBased";
+
+1;
