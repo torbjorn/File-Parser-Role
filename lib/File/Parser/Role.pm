eed38d143e1664e85a58aceadc813172a04d79dc
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2014-12-04 11:30:52 +0100

    name change and email address

--- lib/File/Parser/Role.pm
+++ lib/File/Parser/Role.pm
@@ -251,13 +251,13 @@ L<http://rt.cpan.org>.
 
 =head1 AUTHOR
 
-Torbjørn Lindahl  C<< <torbjorn.lindahl@diagenic.com> >>
+Torbjørn Lindahl  C<< <torbjorn.lindahl@gmail.com> >>
 
 
 =head1 LICENCE AND COPYRIGHT
 
 Copyright (c) 2012, Torbjørn Lindahl C<<
-<torbjorn.lindahl@diagenic.com> >>. All rights reserved.
+<torbjorn.lindahl@gmail.com> >>. All rights reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
